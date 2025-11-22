e4b359fc95f79f7120072090a9a543a80136792d
feichuan <2730879271@qq.com>
2025-11-22 21:50:18 +0800

    🐞 fix(注册模块): 修改部分错误
    0.00.01.22

--- functions/api/register.js
+++ functions/api/register.js
@@ -20,6 +20,7 @@ export async function onRequest(context) {
     const verify_code = formData.get ? formData.get('verify_code') : formData.get('verify_code');
     // 校验 Turnstile token
     const secretKey = env['cf-turnstile'];
+    const turnstileToken = formData.get('cf-turnstile-response');
     const verifyRes = await fetch('https://challenges.cloudflare.com/turnstile/v0/siteverify', {
       method: 'POST',
       headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
